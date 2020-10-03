fff7e262c346cd4b20d0540d548caf8399be7b7b
thesombats <Kaewkamphon.su.62@ubu.ac.th>
2020-10-03 16:06:39 +0000

    10/03/2020

--- myweb/views.py
+++ myweb/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render , redirect
+from .forms import RegisterForm
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Question, Choice
+from django.contrib import messages
 
 # Create your views here.
 
@@ -18,36 +19,61 @@ def home(req):
     return render(req, 'myweb/home.html')
 
 def login_user(req):
-    if req.method == 'POST':
-        user = User.objects.get(username=req.POST['username'])
-        user = authenticate(username=user.username, password=req.POST['password'])
-        if user:
-            login(req, user)
-            return redirect('/home')
-
-        else:
-            return render(req, 'myweb/login.html')
-
+    if req.user.is_authenticated:
+        return redirect('/home')
     else:
-        return render(req, 'myweb/login.html')
+        if req.method == 'POST':
+            username = req.POST.get('username')
+            password = req.POST.get('password')
+
+            user = authenticate(req, username=username, password=password)
+
+            if user is not None:
+                login(req, user)
+                return redirect('/home')
+            else:
+                messages.info(req, 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง')
+
+        context = {}
+        return render(req, 'myweb/login.html', context)
+
 
 def logout_user(req):
     logout(req)
     return redirect("/home")
 
-def register_user(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('/home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'myweb/register.html', {'form': form})
+
+def register_user(response):
+    if response.user.is_authenticated:
+        return redirect('/home')
+
+    else: 
+        form = RegisterForm()
+        if response.method == "POST":
+            username = response.POST.get('username')
+            email = response.POST.get('email')
+            password1 = response.POST.get('password1')
+            password2 = response.POST.get('password2')
+
+            form = RegisterForm(response.POST)
+
+            if form.is_valid():
+                form.save()
+
+                return redirect("/login")
+            
+            elif len(password1) < 8 or len(password2) < 8:
+                messages.info(response, 'รหัสผ่านของคุณต้องมี 8 ตัวอย่างต่ำ')
+                
+            elif password1 != password2:
+                messages.info(response, 'รหัสผ่านของคุณทั้ง 2 อันต้องเหมือนกัน')
+                
+            else:
+                messages.info(response, 'รหัสผ่านของคุณต้องไม่ใช่ตัวเลขทั้งหมด')
+            
+
+    context = {'form':form}
+    return render(response, "myweb/register.html", {"form":form})
 
 
 def detail(request, question_id):
